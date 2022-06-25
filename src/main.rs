ff4da3467c165fd21f670b0ee0118a32c673ac5e
Mayank <mayank.rana@learningyogi.org>
2022-06-25 17:00:31 +0530

    clap added for cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+#![allow(unused)]
+use clap::{Parser, Subcommand};
+
+#[derive(Debug, Subcommand)]
+enum Commands
+{    
+    Record
+    {
+        name: Option<String>
+    },    
+    #[clap(arg_required_else_help = true)]
+    Play
+    {
+        name: String
+    }    
+}
+
+#[derive(Debug, Parser)]
+#[clap(name="record-audio")]
+#[clap(about="simple audio recorder")]
+struct Cli{
+    #[clap(subcommand)]
+    command:Commands,
+}
+
 fn main() {
-    println!("Hello, world!");
+    
+let args = Cli::parse();
+
+match args.command
+    {
+        Commands::Record { name } =>
+        {
+            println!("Record!");
+            todo!()
+        }
+        Commands::Play { name } =>
+        {
+            println!("Play!");
+            todo!()
+        }        
+    };
 }
